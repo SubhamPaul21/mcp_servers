87040612f23020b39aec39ca86f2f5ff86d61d2d
SubhamPaul21 <subhampaul12@gmail.com>
2025-07-31 01:57:53 +0530

    removed gradio code logic for cleaner code base

--- servers/sentiment_analysis_server.py
+++ servers/sentiment_analysis_server.py
@@ -1,19 +1,16 @@
 import json
-import gradio as gr
 from textblob import TextBlob
 from mcp.server.fastmcp import FastMCP
-from pydantic import Field
 
-mcp = FastMCP("SentimentAnalysisMCP", version="1.0.0", log_level="ERROR")
+# from pydantic import Field
+mcp = FastMCP("sentiment-analysis")
 
 
 @mcp.tool(
     name="sentiment_analysis",
-    description="Analyze the sentiment of the input text using TextBlob.",
+    description="Perform sentiment analysis on the input text.",
 )
-def sentiment_analysis(
-    text: str = Field(description="The input text to analyze"),
-) -> str:
+def sentiment_analysis(text: str) -> str:
     """Perform sentiment analysis on the input text.
     Args:
         text (str): The input text to analyze.
@@ -35,18 +32,6 @@ def sentiment_analysis(
     return json.dumps(result)
 
 
-# Create a Gradio interface for the sentiment analysis function
-demo = gr.Interface(
-    fn=sentiment_analysis,
-    inputs=gr.Textbox(
-        label="Input Text", placeholder="Enter text to analyze sentiment..."
-    ),
-    outputs=gr.Textbox(label="Sentiment Analysis Result"),
-    title="Sentiment Analysis",
-    description="Analyze the sentiment of the input text using TextBlob.",
-)
-
-# Launch the Gradio interface
+# Start the server
 if __name__ == "__main__":
-    # demo.launch(mcp_server=True)
     mcp.run(transport="stdio")
